018e89cb8c08cd727522742e1f808fa787807dd9
ekweobac <ekweobac@gmail.com>
2022-09-13 13:29:30 -0700

    Tasks 0, 1 and 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet_x10
+ *
+ * Description: function to print abc 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+		_putchar('\n');
+	}
+{
