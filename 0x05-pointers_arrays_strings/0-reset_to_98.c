3f5ae31d01ccf9f4dd037c86452ba4dd40db1a44
ekweobac <ekweobac@gmail.com>
2022-09-19 17:25:52 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * 0-reset_to_98.c - reset the value of a given integer
+ * to 98
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+ - Pointers, arrays and strings
+ALX Course on C - Pointers, arrays and strings 0x05
