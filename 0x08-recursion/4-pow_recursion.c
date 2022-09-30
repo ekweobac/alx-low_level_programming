8813a99b2b1d326c65e6fc072928941483c6e3da
ekweobac <ekweobac@gmail.com>
2022-09-29 18:36:39 -0700

    hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,4 +7,12 @@
  * Return: x and y
  */
 
-int _pow_re
+int _pow_recursion(int x, int y)
+
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
